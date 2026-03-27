920f6031661e6ce89c702f5e4e1ba3762ff47a55
LWDJD\15081 <lwdjd207@163.com>
2026-03-27 09:58:49 +0800

    优化代码结构，将Loc变量由ipfar.go移入config.go

--- config/config.go
+++ config/config.go
@@ -1,5 +1,9 @@
 package config
 
+import (
+	"github.com/leonelquinteros/gotext"
+)
+
 // Config 主配置结构体
 type Config struct {
 	Language string `json:"language"`
@@ -7,3 +11,6 @@ type Config struct {
 
 // ConfigFile 全局配置文件实例
 var ConfigFile *Config
+
+// Loc 全局语言实例
+var Loc *gotext.Locale
